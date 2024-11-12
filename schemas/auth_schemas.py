ecdc52ed23c2e1ba788019d0c6afcdb3757e8bf4
Joshua-RG <arguerrero06gmail.com>
2024-11-11 23:18:49 -0500

    modificacion de respuesta del Token

--- schemas/auth_schemas.py
+++ schemas/auth_schemas.py
@@ -1,5 +1,10 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+    user_id: Optional[int] = None
+    user_name: Optional[str] = None
+    user_email: Optional[str] = None
+    user_role: Optional[str] = None
